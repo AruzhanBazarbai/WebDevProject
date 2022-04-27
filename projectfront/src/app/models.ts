2a42e3b90378e68364e2639f03716f50e5da715b
aishadautova <ai_dautova@kbtu.kz>
2022-04-27 22:40:45 +0600

    commit

--- projectfront/src/app/models.ts
+++ projectfront/src/app/models.ts
@@ -6,11 +6,7 @@ export interface Product{
     name: string;
     description: string;
     cost: number;
-<<<<<<< HEAD
     category: number;
-    image: string;
-=======
     img: string;
     category_id: number;
->>>>>>> b958e8cb1b8c2265ceaf0d44952252ad4e0ce40b
 }
