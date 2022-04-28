83177573abeec1537aff5f1fb4e5a3738993e764
aishadautova <ai_dautova@kbtu.kz>
2022-04-28 14:37:11 +0600

    commit

--- projectfront/src/app/top-bar/top-bar.component.ts
+++ projectfront/src/app/top-bar/top-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from '../category';
 import { CategoryService } from '../category.service';
+import { City } from '../models';
 
 @Component({
   selector: 'app-top-bar',
@@ -10,16 +11,24 @@ import { CategoryService } from '../category.service';
 export class TopBarComponent implements OnInit{
 
   categories: Category[]=[];
+  cities: City[]=[];
 
   constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.getCategories();
+    this.getCities();
   }
 
   getCategories(){
     this.categoryService.getCategories().subscribe((data)=>{
       this.categories = data;
     });
+  }
+
+  getCities(){
+    this.categoryService.getCities().subscribe((data)=>{
+      this.cities = data;
+    })
   }
 }
